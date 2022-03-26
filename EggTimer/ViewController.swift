d3c62a71d2930e5ef3cee410bd3ff65117136314
dbae82 <dbae82@gmail.com>
2022-03-26 14:41:26 -0500

    created timer and conditional when timer ends title changes

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var counter = 0
+    
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        print(eggTimes[hardness]!)
+        counter = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
     }
     
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds.")
+            counter -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
 }
